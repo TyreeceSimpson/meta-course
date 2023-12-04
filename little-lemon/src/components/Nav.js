4647b3f373632b9787a6c57e27da069aaa4b7c3a
Tyreece-Jay <43005542+TyreeceSimpson@users.noreply.github.com>
2023-12-04 15:04:50 +0000

    footer

--- little-lemon/src/components/Nav.js
+++ little-lemon/src/components/Nav.js
@@ -1,15 +1,15 @@
-import styles from './Nav.module.css';
+import styles from './Nav.module.css'
 
 export function Nav() {
   return (
     <nav>
       <ul className={styles.nav}>
-        <li><a href="/home" className={styles.link}>Home</a></li>
-        <li><a href="/about" className={styles.link}>About</a></li>
-        <li><a href="/menu" className={styles.link}>Menu</a></li>
-        <li><a href="/reservations" className={styles.link}>Reservations</a></li>
-        <li><a href="/order" className={styles.link}>Order Online</a></li>
-        <li><a href="/login" className={styles.link}>Login</a></li>
+        <li><a className={styles.link} href="/home">Home</a></li>
+        <li><a className={styles.link} href="/about">About</a></li>
+        <li><a className={styles.link} href="/menu">Menu</a></li>
+        <li><a className={styles.link} href="/reservations">Reservations</a></li>
+        <li><a className={styles.link} href="/order">Order Online</a></li>
+        <li><a className={styles.link} href="/login">Login</a></li>
       </ul>
     </nav>
   )
--- little-lemon/src/sections/Footer.js
+++ little-lemon/src/sections/Footer.js
@@ -1,5 +1,28 @@
+import styles from './Footer.module.css'
+import image from '../icons_assets/Logo.svg'
+
 export function Footer() {
   return (
-    <footer></footer>
+    <footer className={styles.footer}>
+      <img src={image} alt="Image" className={styles.image} />
+      <ul className={styles.list}>
+        <li><a className={styles.link} href="/home">Home</a></li>
+        <li><a className={styles.link} href="/about">About</a></li>
+        <li><a className={styles.link} href="/menu">Menu</a></li>
+        <li><a className={styles.link} href="/reservations">Reservations</a></li>
+        <li><a className={styles.link} href="/order">Order Online</a></li>
+        <li><a className={styles.link} href="/login">Login</a></li>
+      </ul>
+      <ul className={styles.list}>
+        <li className={styles.text}>1 Lemon Street, Chicago</li>
+        <li className={styles.text}>98463 749372</li>
+        <li className={styles.text}>help@littlelemon.com</li>
+      </ul>
+      <ul className={styles.list}>
+        <li><a href="#" className={styles.link}>X</a></li>
+        <li><a href="#" className={styles.link}>Facebook</a></li>
+        <li><a href="#" className={styles.link}>Instagram</a></li>
+      </ul>
+    </footer>
   )
 }
