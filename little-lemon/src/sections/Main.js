13f08c9ffe432968fd413629573e4669c4837634
Tyreece-Jay <43005542+TyreeceSimpson@users.noreply.github.com>
2023-12-04 16:49:44 +0000

    hero

--- little-lemon/src/sections/Main.js
+++ little-lemon/src/sections/Main.js
@@ -1,5 +1,18 @@
+import styles from './Main.module.css'
+import image from '../icons_assets/restauranfood.jpg'
+
 export function Main() {
   return (
-    <main></main>
+    <main>
+      <section className={styles.hero}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Little Lemon</h1>
+          <h2 className={styles.subtitle}>Chicago</h2>
+          <p className={styles.text}>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+          <button className={styles.button}>Order Now</button>
+        </div>
+        <img src={image} alt="Image" className={styles.image} />
+      </section>
+    </main>
   )
 }
