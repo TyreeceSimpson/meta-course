332c1b3d82737831b557d7267be7fe5d87d51c80
Tyreece-Jay <43005542+TyreeceSimpson@users.noreply.github.com>
2023-12-05 12:25:20 +0000

    .

--- little-lemon/src/components/organisms/About.js
+++ little-lemon/src/components/organisms/About.js
@@ -7,7 +7,6 @@ export function About() {
       <div className={styles.content}>
         <h1 className={styles.title}>Little Lemon</h1>
         <h2 className={styles.subtitle}>Chicago</h2>
-        <p className={styles.text}>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
       </div>
       <img src={image} alt="Food" className={styles.image} />
     </section>
