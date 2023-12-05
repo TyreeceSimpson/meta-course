285f36fc183d9020c5afbfda3dc8464237c29876
Tyreece-Jay <43005542+TyreeceSimpson@users.noreply.github.com>
2023-12-05 16:59:27 +0000

    .

--- little-lemon/src/pages/Reservations.js
+++ little-lemon/src/pages/Reservations.js
@@ -1,8 +1,10 @@
 import { BookingForm } from "../organisms/BookingForm"
+import { Header } from '../organisms/Header'
 
 export function Reservations() {
   return (
     <>
+      <Header />
       <h1>Reservations</h1>
       <BookingForm />
     </>
