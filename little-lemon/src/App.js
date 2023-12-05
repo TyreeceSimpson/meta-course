d9011bdea5d739821cd3262c81895108b185b7e3
Tyreece-Jay <43005542+TyreeceSimpson@users.noreply.github.com>
2023-12-05 16:24:15 +0000

    .

--- little-lemon/src/App.js
+++ little-lemon/src/App.js
@@ -5,7 +5,10 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />}></Route>
+      <Route path="/menu" element={<Home />}></Route>
       <Route path="/reservations" element={<Home />}></Route>
+      <Route path="/order" element={<Home />}></Route>
+      <Route path="/login" element={<Home />}></Route>
     </Routes>
   )
 }
