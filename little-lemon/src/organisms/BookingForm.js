6a72ec6aea83a5d9719a7313449e4f87d969b635
Tyreece-Jay <43005542+TyreeceSimpson@users.noreply.github.com>
2023-12-05 18:23:10 +0000

    .

--- little-lemon/src/organisms/BookingForm.js
+++ little-lemon/src/organisms/BookingForm.js
@@ -1,21 +1,29 @@
+import { useState, initialState } from "react"
+
 export function BookingForm() {
+  const [availableTimes, setAvailableTimes] = useState(["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"])
+  const [date, setDate] = useState(initialState)
+  const [time, setTime] = useState(initialState)
+  const [guests, setGuests] = useState(initialState)
+  const [occasion, setOccasion] = useState(initialState)
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    alert(`${date}, ${time}, ${guests}, ${occasion}`)
+  }
+
   return (
-    <form>
-      <label for="res-date">Choose date</label>
-      <input type="date" id="res-date" />
-      <label for="res-time">Choose time</label>
-      <select id="res-time ">
-        <option>17:00</option>
-        <option>18:00</option>
-        <option>19:00</option>
-        <option>20:00</option>
-        <option>21:00</option>
-        <option>22:00</option>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="res-date">Choose date</label>
+      <input type="date" id="res-date" onChange={e => setDate(e.target.value)} />
+      <label htmlFor="res-time">Choose time</label>
+      <select id="res-time" onChange={e => setTime(e.target.value)}>
+        {availableTimes.map((time) => <option>{time}</option>)}
       </select>
-      <label for="guests">Number of guests</label>
-      <input type="number" placeholder="1" min="1" max="10" id="guests" />
-      <label for="occasion">Occasion</label>
-      <select id="occasion">
+      <label htmlFor="guests">Number of guests</label>
+      <input type="number" placeholder="1" min="1" max="10" id="guests" onChange={e => setGuests(e.target.value)} />
+      <label htmlFor="occasion">Occasion</label>
+      <select id="occasion" onChange={e => setOccasion(e.target.value)}>
         <option>Birthday</option>
         <option>Anniversary</option>
       </select>
--- little-lemon/src/pages/Reservations.js
+++ little-lemon/src/pages/Reservations.js
@@ -1,11 +1,12 @@
 import { BookingForm } from "../organisms/BookingForm"
 import { Header } from '../organisms/Header'
+import styles from "../organisms/styles/BookingForm.module.css"
 
 export function Reservations() {
   return (
     <>
       <Header />
-      <h1>Reservations</h1>
+      <h1 className={styles.title}>Reservations</h1>
       <BookingForm />
     </>
   )
